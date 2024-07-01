3e8ecb44a7a303200f0e11153029fe0a8a2f21fb
Hannah <57945088+HannahPadd@users.noreply.github.com>
2024-07-01 22:23:11 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,19 +2,23 @@
   <div class="home">
     <h2>Welcome to Gimmick Vortex!</h2>
     <p>You can either upload a stepfile and start editing it or create a new one</p>
+    <UploadComponent />
   </div>
-  <div class="content">
+  <div class="editor">
     <ChartEditor />
   </div>
+  <div class="upload"></div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import ChartEditor from '@/components/ChartEditor.vue'
+import UploadComponent from '@/components/UploadComponent.vue'
 
 export default defineComponent({
   components: {
-    ChartEditor
+    ChartEditor,
+    UploadComponent
   }
 })
 </script>
